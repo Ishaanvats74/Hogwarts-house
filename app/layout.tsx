2362488aedf7b7d687112df7470481c17ea36974
Ishaan vats <Ishaanvats74@gmail.com>
2025-07-17 03:16:30 +0530

    home page changes done and vercel Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Analytics/>
       </body>
     </html>
   );
